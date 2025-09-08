e4e6b1361435939d47310c32408a780d87591a85
Daniel Workneh <daniel.workineh27@gmail.com>
2025-09-08 12:36:05 +0300

    Week 3 Assignment: Data Manipulation and Transactions

--- answers.sql
+++ answers.sql
@@ -1,1 +1,22 @@
 -- Week 3 database Assignment
+
+            -- Question1 --
+
+Create Table student(
+id INT PRIMARY KEY,
+fullName VARCHAR(100),
+age INT
+);
+
+            -- Question2 --
+
+INSERT INTO student(id, fullName, age) 
+VALUES (1, 'Teferi Mekonnen', 23),
+(2, 'Mengistu Hailemariam', 18),
+(3, 'Kidanewold Kiflie', 19);
+
+            -- Question 3 --
+UPDATE student
+SET age = 20
+WHERE id = 2;
+
